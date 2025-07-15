74470a97d00574d2c7e06fd1f2f2b487939eb48f
eve.cs <eve.chunyanan.s@gmail.com>
2025-07-15 12:06:25 +0700

    task01 solution

--- task01.js
+++ task01.js
@@ -1,5 +1,10 @@
-"use strict"
+"use strict";
 
 // TODO: Use if/else to print "Even" or "Odd"
 
 const number = 15;
+if (number % 2 === 0) {
+  console.log("number is an Even Number");
+} else {
+  console.log("number is an Odd Number");
+}
--- task02.js
+++ task02.js
@@ -5,3 +5,36 @@
 
 const userInput = "42";
 // Try changing to "hello", 10, null, NaN
+
+if (typeof userInput === "number" && !isNaN(userInput)) {
+  console.log("userInput is a Number");
+} else {
+  console.log("userInput is not a Number");
+}
+
+let alternateUserInput = "hello";
+if (typeof alternateUserInput === "number" && !isNaN(userInput)) {
+  console.log("alternateUserInput is a Number");
+} else {
+  console.log("alternateUserInput is not a Number");
+}
+
+let tenUserInput = 10;
+if (typeof tenUserInput === "number" && !isNaN(tenUserInput)) {
+  console.log("tenUserInput is a Number");
+} else {
+  console.log("tenUserInput is not a Number");
+}
+let nullUserInput = null;
+if (typeof nullUserInput === "number" && !isNaN(nullUserInput)) {
+  console.log("nullUserInput is a Number");
+} else {
+  console.log("nullUserInput is not a Number");
+}
+
+let naNUserInput = NaN;
+if (typeof naNUserInput === "number" && !isNaN(naNUserInput)) {
+  console.log("naNUserInput is a Number");
+} else {
+  console.log("anaNUserInput is not a Number");
+}
